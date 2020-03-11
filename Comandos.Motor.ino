1761f220927dbdc7e1ba2b4095877daf51effa34
oriolnba <56670633+oriolnba@users.noreply.github.com>
2020-03-11 21:26:13 +0100

    Update Comandos.Motor.ino

--- Comandos.Motor.ino
+++ Comandos.Motor.ino
@@ -23,10 +23,73 @@ void loop()
   
 }
 
-void Adelante()
-{ 
-   digitalWrite(pinD1,HIGH);
-   digitalWrite(pinI2,HIGH);  
-}       
-   
+void moveStop()
+{
+  digitalWrite(pinD1, LOW);
+  digitalWrite(pinI2, LOW);
+  digitalWrite(pinD2, LOW);
+  digitalWrite(pinI1, LOW);
+}
+
+void moveForward(){
+
+  if(!goesForward){
+
+    goesForward=true;
     
+    digitalWrite(pinI2, HIGH);
+    digitalWrite(pinD1, HIGH);
+  
+    digitalWrite(pinI1, LOW);
+    digitalWrite(pinD2, LOW); 
+  }
+}
+
+void moveBackward(){
+
+  goesForward=false;
+
+  digitalWrite(pinI1, HIGH);
+  digitalWrite(pinD2, HIGH);
+  
+  digitalWrite(pinI2, LOW);
+  digitalWrite(pinD1, LOW);
+  
+}
+
+void turnRight(){
+
+  digitalWrite(pinI2, HIGH);
+  digitalWrite(pinD2, HIGH);
+  
+  digitalWrite(pinI1, LOW);
+  digitalWrite(pinD1, LOW);
+  
+  delay(500);
+  
+  digitalWrite(pinI2, HIGH);
+  digitalWrite(pinD1, HIGH);
+  
+  digitalWrite(pinI1, LOW);
+  digitalWrite(pinD2, LOW);
+ 
+  
+  
+}
+
+void turnLeft(){
+
+  digitalWrite(pinI1, HIGH);
+  digitalWrite(pinD1,HIGH);
+  
+  digitalWrite(pinI2, LOW);
+  digitalWrite(pinD2, LOW);
+
+  delay(500);
+  
+  digitalWrite(pinI2, HIGH);
+  digitalWrite(pinD1, HIGH);
+  
+  digitalWrite(pinI1, LOW);
+  digitalWrite(pinD2, LOW);
+}
